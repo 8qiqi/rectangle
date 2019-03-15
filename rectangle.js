862e8d6daa444c113696b4c56fd6fa53528b2f72
8qiqi <3510363360@qq.com>
2019-03-15 11:29:46 +0800

    任务12

--- rectangle.js
+++ rectangle.js
@@ -1,19 +1,23 @@
-$(function(){
+/* global Rectangle: true */
+$(function() {
   var $width = $('#width'),
       $height = $('#height'),
-      $btnCal = $('#calculate'),
-      $perimeter = $('#perimeter'),
-      $area = $('#arear');
+@@ -6,13 +7,12 @@ $(function() {
+      $area = $('#area');
 
   $btnCal.click(function(){
     var w = Number($width.val()),
         h = Number($height.val());
+    var w = $width.val(),
+        h = $height.val();
 
-  var p = 2*(w+h),
-      a = w*h;
+    var p = 2 * (w + h),
+        a = w * h;
+    var r = new Rectangle(w, h);
 
-  $perimeter.val(p);
-  $area.val(a);
+    $perimeter.val(p);
+    $area.val(a);
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
   });
 });
-
