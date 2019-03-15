848418b7b4b7e5753e3da311c0924e4cd69457ce
8qiqi <3510363360@qq.com>
2019-03-15 12:44:50 +0800

    任务16

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,35 +17,47 @@ module.exports = function (grunt) {
         src: '*.css'
         
   },
-  htmlhint: {
+  htmlmin: {
     options: {
-              htmlhintrc: '.htmlhintrc'
-          
+              collapseWhitespace: true,
+          preserveLineBreaks: false
+                  
     },
-          src: '*.html'
-                
-  },
-  mocha: {
-    test: {
-              src: ['test/index.html'],
-                    
-    },
-    options: {
-              run: true,
-                      reporter: 'Spec'
+    files: {
+              src: './index.html',
+                      dest: 'dist/index.html'
                               
     }
         
+  },
+  cssmin: {
+          'dist/rectangle.css': 'rectangle.css'
+                
+  },
+  uglify: {
+    release:{
+      files: {
+                  'dist/rectangle.js': 'rectangle.js',
+                            'dist/calc.js': 'calc.js'
+                                      
+      }
+                   
+    }
+                
   }     
     
   });
 
     grunt.loadNpmTasks('grunt-contrib-csslint');
-      grunt.loadNpmTasks('grunt-htmlhint');
-        grunt.loadNpmTasks('grunt-eslint');
-        grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+    grunt.registerTask('unitTest', ['mocha']);
+    grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
 
-          grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
-          grunt.registerTask('unitTest', ['mocha']);
 
 };
